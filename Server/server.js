4be3e0e9c321a2b96105a7cdc4477b279184f74c
shkurteRe <104673482+shkurteRe@users.noreply.github.com>
2023-04-28 15:34:16 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -68,7 +68,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
